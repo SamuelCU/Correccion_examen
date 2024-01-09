7b7a7276a62da7c92d3536ad4565d5124960a2ff
Samuel Cuti <samcuti.03@gmail.com>
2024-01-08 20:26:13 -0500

    Version 2.0

--- Examen_correccion/src/Main.java
+++ Examen_correccion/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         String [] musicaPop = new String[3];
-        String [] musicaRock = new String[1];
+        String [] musicaRock = new String[3];
 
 
         Baladapop musicaPop1 = new Baladapop("Eleanor Rigby", "4 personas con instrumentos", "Canto pop");
@@ -19,13 +19,31 @@ public class Main {
             musicaPop[0] = musicaPop1.getNombre_musica();
             musicaPop[1] = musicaPop1.getPresentacionEscenario();
             musicaPop[2] = musicaPop1.getTipoCanto();
+
             System.out.println("\n\nMusica numero 1:");
             for (int i = 0; i < musicaPop.length; i++) {
 
                 System.out.println(musicaPop[i]);
 
             }
+            musicaPop1.imprimirOrigen();
+            musicaPop1.grupoPrincipal();
         }
+        musicaRock1.setInstrumento_principal("Guitarra");
+        musicaRock[0] = musicaRock1.getNombre_musica();
+        musicaRock[1] = musicaRock1.getMarcaGuitarra();
+        musicaRock[2] = musicaRock1.getInstrumento_principal();
+
+        System.out.println("------------------------------------------------");
+        System.out.println("\nMusica numero 2:");
+        for(int i = 0; i< musicaRock.length;i++){
+            System.out.println(musicaRock[i]);
+        }
+
+        musicaRock1.imprimirOrigen();
+        musicaRock1.reyRock();
+
+
 
 
 
